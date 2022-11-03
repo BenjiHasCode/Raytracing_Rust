e76640fc0a46fb580f40ca4371a84b6032a5d299
BenjiHasCode <benj1558@gmail.com>
2022-11-03 07:24:49 +0100

    added fuzz to metal

--- src/main.rs
+++ src/main.rs
@@ -38,8 +38,8 @@ fn main() {
 
     let material_ground = Material::Lambertian(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
     let material_center = Material::Lambertian(Lambertian::new(Color::new(0.7, 0.3, 0.3)));
-    let material_left = Material::Metal(Metal::new(Color::new(0.8, 0.8, 0.8)));
-    let material_right = Material::Metal(Metal::new(Color::new(0.8, 0.6, 0.2)));
+    let material_left = Material::Metal(Metal::new(Color::new(0.8, 0.8, 0.8), 0.3));
+    let material_right = Material::Metal(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0));
 
     world.add(Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0, material_ground)));
     world.add(Box::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5, material_center)));
--- src/material/metal.rs
+++ src/material/metal.rs
@@ -1,13 +1,14 @@
 use crate::{vec3::{Color, Vec3}, ray::Ray, hittable::HitRecord};
 
 pub struct Metal {
-    albedo: Color
+    albedo: Color,
+    fuzz: f64
 }
 
 impl Metal {
     pub fn scatter(&self, r_in: &Ray, rec: &HitRecord, attenuation: &mut Color, scattered: &mut Ray) -> bool {
         let reflected = Vec3::reflect(&r_in.direction().unit_vector(), &rec.normal);
-        *scattered = Ray::new(rec.p, reflected);
+        *scattered = Ray::new(rec.p, reflected + self.fuzz*Vec3::random_in_unit_sphere());
         *attenuation = self.albedo;
         
         scattered.direction().dot(&rec.normal) > 0.0
@@ -15,7 +16,7 @@ impl Metal {
 }
 
 impl Metal {
-    pub fn new(albedo: Color) -> Metal {
-        Metal { albedo }
+    pub fn new(albedo: Color, fuzz: f64) -> Metal {
+        Metal { albedo, fuzz }
     }
 }
